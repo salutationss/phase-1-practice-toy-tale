4172220455a31ab3e37291b86caad62ee45d9c05
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-05-27 18:52:24 -0400

    commit tree 28796eb35ce86182991f55bf5e84c32224c2098b

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
 let addToy = false;
+let divCollect = document.querySelector('#toy-collection')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+
+function getToy() {
+  return fetch('http://localhost:3000/toys')
+  .then (res => res.json())
+}
+
+function postToy(toy_data) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json' ,
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": toy_data.name.value,
+      "image": toy_data.image.value,
+      "likes": 0
+    })
+  })
+  .then(res => res.json())
+  .then((obj_toy) => {
+    let new_toy = renderToys(obj_toy)
+    divCollect.append(new_toy)
+  })
+}
+
